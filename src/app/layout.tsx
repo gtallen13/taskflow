b495dfb4ffa30a4cada6c402b82a32760e78f9f9
Thomas Allen <gtallenpadi13@gmail.com>
2026-06-12 22:12:43 -0600

    Changin font style

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,14 +3,26 @@ import { AuthProvider } from "@/context/AuthContext";
 import { Toaster } from "sonner";
 import "./globals.css";
 
+
+import { Poppins } from "next/font/google";
+const poppins = Poppins({
+  subsets: ["latin"],
+  weight: ["400", "500", "600", "700"],
+});
+
+
+
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+
+
+
   return (
     <html lang="en">
-      <body>
+      <body className={poppins.className}>
         <AuthProvider>
           {children}
           <Toaster richColors />
